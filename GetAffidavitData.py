16371bf98803d5fcec6976c2c56024f0c8ab2d5c
Srikanth L <code@srik.me>
2021-08-11 14:04:59 +0530

    Update Accepted Candidates with affidavit link

--- GetAffidavitData.py
+++ GetAffidavitData.py
@@ -1,5 +1,7 @@
+import base64
 import json
 import logging
+import os
 import time
 
 import requests
@@ -62,14 +64,74 @@ def getCandidatesInElection(season_code, election_code):
                season_code + '__' + election_code + '.json')
 
 
-def main():
-    logging.basicConfig(filename='AffadavitExtractor' + time.strftime("%Y%m%d-%H%M%S") +
-                        '.log', format='%(asctime)s %(message)s', level=logging.INFO)
+def getAffidavitLink(candidate):
+    logging.debug("====getAffidavitLink===" + candidate['link'])
+    s = BeautifulSoup(requests.get(candidate['link']).text, 'lxml')
+    candidate['affidavit_upload_date'] = s.find(
+        'div', attrs={'class': 'aside-af'}).find_all('span')[-1].text.strip()
+    candidate['affidavit_upload_count'] = s.find(
+        'span', attrs={'id': 'updateCount'}).text
+    candidate['nomination_file_date'] = s.find('div', attrs={'class': 'col-md-5 col-sm-6 mt-5 party'}).find_all(
+        'div', attrs={'class': 'row'})[6].find_all('div', attrs={'class': 'col-sm-6'})[1].text.strip()
+    candidate_bio = s.find('form', attrs={
+                           'class': 'form-horizontal'}).find_all('div', attrs={'class': 'form-group'})
+    candidate['father_husband_name'] = candidate_bio[0].find_all(
+        'div', attrs={'class': 'col-sm-6'})[0].text.strip()
+    candidate['address'] = candidate_bio[4].find_all(
+        'div', attrs={'class': 'col-sm-6'})[0].text.strip()
+    candidate['gender'] = candidate_bio[5].find_all(
+        'div', attrs={'class': 'col-sm-6'})[0].text.strip()
+    candidate['age'] = candidate_bio[6].find_all(
+        'div', attrs={'class': 'col-sm-6'})[0].text.strip()
+    try:
+        candidate['affidavit_link'] = base64.b64decode(s.find(
+            'div', attrs={'class': 'aside-af'}).find('input', attrs={'id': 'pdfUrl'})['value']).decode('utf-8')
+    except:
+        candidate['affidavit_link'] = 'NOT AVAILABLE'
+        return candidate
+    return candidate
+
+
+def getCandidateProfiles():
+    logging.debug("====getCandidateProfiles===")
     with open("elections.json") as json_file:
         data = json.load(json_file)
     for season in data:
         for election in season['elections']:
             getCandidatesInElection(season['code'], election)
+
+
+def getAffidavitDetails():
+    logging.debug("====getAffidavitDetails===")
+    json_folder_path = os.path.abspath(os.getcwd()) + '/data'
+    json_files = [x for x in os.listdir(
+        json_folder_path) if x.endswith("json")]
+
+    affidavit_accepted_all = []
+    for json_file in json_files:
+        logging.info("Processing --- " + json_file)
+        json_file_path = os.path.join(json_folder_path, json_file)
+        with open(json_file_path, "r") as f:
+            data = json.load(f)
+
+        for candidate in data:
+            if candidate['status'] != 'Accepted':
+                continue
+            try:
+                candidate['election_code'] = json_files[1].split('__')[1][:-5]
+                affidavit_accepted_all.append(getAffidavitLink(candidate))
+            except:
+                logging.info("Other Exception getting AffidavitLink for candidate " +
+                             candidate['name'] + " using link " + candidate['link'])
+                pass
+    write_json(affidavit_accepted_all, 'affidavits_accepted_all.json')
+
+
+def main():
+    logging.basicConfig(filename='AffadavitExtractor' + time.strftime("%Y%m%d-%H%M%S") +
+                        '.log', format='%(asctime)s %(message)s', level=logging.INFO)
+    getCandidateProfiles()
+    getAffidavitDetails()
 
 
 if __name__ == "__main__":
